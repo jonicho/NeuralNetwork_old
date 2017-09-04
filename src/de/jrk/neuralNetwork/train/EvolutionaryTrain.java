ba2c4ce85c241553ed02032aa8333c30e7d9d890
Jonas Keller <jonas.richardd@gmail.com>
2017-09-04 19:03:27 +0200

    finish evolutionary train

--- src/de/jrk/neuralNetwork/train/EvolutionaryTrain.java
+++ src/de/jrk/neuralNetwork/train/EvolutionaryTrain.java
@@ -1,6 +1,8 @@
 package de.jrk.neuralNetwork.train;
 
 import java.io.InputStream;
+import java.io.OutputStream;
+
 import de.jrk.neuralNetwork.NeuralNetwork;
 
 public class EvolutionaryTrain {
@@ -16,7 +18,10 @@ public class EvolutionaryTrain {
 		scores = new float[this.networksPerGen];
 		
 		networks[0] = new NeuralNetwork(loadLevel, inputStream);
-		mutateNetworksFromFirstNetwork();
+		for (int i = 1; i < networks.length; i++) {
+			networks[i] = networks[0].getClone();
+			networks[i].mutate();
+		}
 	}
 	
 	public float[] step(float[] inputs) {
@@ -31,19 +36,31 @@ public class EvolutionaryTrain {
 		currentNetwork++;
 		if (currentNetwork >= networksPerGen) {
 			currentNetwork = 0;
-			putBestNetworkToBeginning();
-			mutateNetworksFromFirstNetwork();
+			mutateNetworksFromBestNetwork();
+			resetScores();
 		}
 	}
 	
-	private void putBestNetworkToBeginning() {
-		int bestNetwork = 0;
-		for (int i = 1; i < networks.length; i++) {
-			//TODO
+	public void saveBestNetwork(int saveLevel, OutputStream outputStream) {
+		networks[getIndexOfBestNetwork()].save(saveLevel, outputStream);
+	}
+	
+	private void resetScores() {
+		for (int i = 0; i < scores.length; i++) {
+			scores[i] = 0;
 		}
 	}
+	
+	private int getIndexOfBestNetwork() {
+		int bestNetwork = 0;
+		for (int i = 1; i < networks.length; i++) {
+			if (scores[i] > scores[bestNetwork]) bestNetwork = i;
+		}
+		return bestNetwork;
+	}
 
-	private void mutateNetworksFromFirstNetwork() {
+	private void mutateNetworksFromBestNetwork() {
+		networks[0] = networks[getIndexOfBestNetwork()];
 		for (int i = 1; i < networks.length; i++) {
 			networks[i] = networks[0].getClone();
 			networks[i].mutate();
