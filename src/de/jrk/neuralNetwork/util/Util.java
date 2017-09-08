6cb873800c2a8ff698f47c1e75f95c62c3abf9de
Jonas Keller <jonas.richardd@gmail.com>
2017-09-08 22:36:09 +0200

    remove unused function

--- src/de/jrk/neuralNetwork/util/Util.java
+++ src/de/jrk/neuralNetwork/util/Util.java
@@ -6,12 +6,4 @@ public class Util {
 	public static Random random = new Random();
 	
 	private Util() {}
-	
-	public static int[] convertToIntArray(String[] strArr) {
-		int[] intArr = new int[strArr.length];
-		for(int i = 0; i < strArr.length; i++) {
-			intArr[i] = Integer.parseInt(strArr[i]);
-		}
-		return intArr;
-	}
 }
